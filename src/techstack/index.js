2fd35ed317955abb4db8493970aac86b0718b1be
Thuwarakesh Murallie <thuwarakesh@gmail.com>
2019-06-14 08:30:07 +0530

    teckstack image changed and large images removed

--- src/techstack/index.js
+++ src/techstack/index.js
@@ -3,4 +3,4 @@ require('./style.scss');
 document.querySelector('.hero').setAttribute('style', `background: linear-gradient(
     rgba(0, 0, 0, 0.5),
     rgba(0, 0, 0, 0.5)
-  ),url('/static/web/img/cover-techstack.jpg') center/cover;`)
+  ),url('/static/web/img/cover-techstack.webp') center/cover;`)
